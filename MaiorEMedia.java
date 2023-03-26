f8fabe1a3b7045b3a4167c2caf6c198aabcddb9f
Talita Rocha <tali_talitarocha@hotmail.com>
2023-03-26 15:50:10 -0300

    Exercício 3: média da soma de 5 nros; maior deles

---
+++ MaiorEMedia.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class MaiorEMedia {
+    /*
+     * Faça um programa que leia 5 números
+     * e informe o maior número e a média
+     * desses números.
+     */
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int numero;
+        int maior = 0;
+        int soma = 0;
+
+        int count = 0;
+        do {
+            System.out.println("Número: ");
+            numero = scan.nextInt();
+
+            soma = soma + numero;
+
+            if (numero > maior)
+                maior = numero;
+
+            count = count + 1;
+        } while (count < 5);
+
+        System.out.println("O maior número digitado é: " + maior);
+        System.out.println("A média dos números digitados é: " + (soma/5));
+
+    }
+}
