11184bfac69d2b2333ea31e3fa1398de0b5c36a2
Talita Rocha <tali_talitarocha@hotmail.com>
2023-03-26 19:40:39 -0300

    Exercício 4: quantidade de números ímpares e pares

---
+++ ImparPar.java
@@ -0,0 +1,7 @@
+public class ImparPar {
+    /*
+     * Faça um programa que solicite N números inteiros,
+     * calcule e mostre a quantidade de números pares
+     * e a quantidade de números ímpares.
+     */
+}
