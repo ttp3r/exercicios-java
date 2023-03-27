68bce3993c091d54eee4dbed6dd5e001a7548895
Talita Rocha <tali_talitarocha@hotmail.com>
2023-03-26 21:58:15 -0300

    Adicionando quantidade de consoantes

--- Ex8Consoantes.java
+++ Ex8Consoantes.java
@@ -36,7 +36,7 @@ public class Ex8Consoantes {
             if (consoante != null)
                 System.out.print(consoante + " ");            
          }
-                 System.out.println("\nQuantidade de consoantes: " + quantidadeConsoantes);
+         System.out.println("\nQuantidade de consoantes: " + quantidadeConsoantes);
 
     }
 }
