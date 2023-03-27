7f7880071e9bad0f49b99ee18289bf4c095258ee
Talita Rocha <tali_talitarocha@hotmail.com>
2023-03-26 22:16:59 -0300

    Exercício 9: gerador de números aleatórios

--- Ex9NumerosAleatorios.java
+++ Ex9NumerosAleatorios.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Ex9NumerosAleatorios {
     /*
      * Faça um programa que leia 20 números aleatórios
@@ -6,6 +8,26 @@ public class Ex9NumerosAleatorios {
      */
 
      public static void main(String[] args) {
-        
+        Random random = new Random();
+
+        int[] numerosAleatorios = new int[20];
+
+        for(int i = 0; i < numerosAleatorios.length; i++) {
+            int numero = random.nextInt(100);
+            numerosAleatorios[i] = numero;
+        }
+
+        System.out.println("Números Aleatórios: ");
+        for (int numero : numerosAleatorios) {
+            System.out.print(numero + " ");
+            
+        }
+
+        System.out.println("\nSucessores dos Números Aleatórios: ");
+        for (int numero : numerosAleatorios) {
+            System.out.print((numero+1) + " ");
+            
+        }
+
      }
 }
