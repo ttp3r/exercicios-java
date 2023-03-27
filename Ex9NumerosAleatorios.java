e4e76d987e40023a7e6250b4d122a86872037ebe
Talita Rocha <tali_talitarocha@hotmail.com>
2023-03-26 22:04:41 -0300

    Criando o exercício 9

---
+++ Ex9NumerosAleatorios.java
@@ -0,0 +1,11 @@
+public class Ex9NumerosAleatorios {
+    /*
+     * Faça um programa que leia 20 números aleatórios
+     * entre 0 e 100 e aemazene-os em um vetor.
+     * Ao final, mostre os números e seus sucessores.
+     */
+
+     public static void main(String[] args) {
+        
+     }
+}
